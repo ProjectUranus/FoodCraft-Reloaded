92a360d546a4756886bbb81f5c9899cf08d52f4a
Takakura Anri <takakura_anri@hotmail.com>
2020-03-20 20:33:43 +0800

    ...and a silly bug of git

---
+++ src/main/java/com/projecturanus/foodcraft/common/util/observable/ObservableItemHandler.kt
@@ -0,0 +1,31 @@
+package com.projecturanus.foodcraft.common.util.observable
+
+import net.minecraft.item.ItemStack
+import net.minecraft.util.NonNullList
+import net.minecraftforge.items.ItemStackHandler
+import org.cyclops.commoncapabilities.api.capability.inventorystate.IInventoryState
+
+class ObservableItemHandler : ItemStackHandler, IInventoryState {
+    val loadListener: MutableList<() -> Unit> = mutableListOf()
+    val contentChangedListener: MutableList<(Int) -> Unit> = mutableListOf()
+    var hashInternal: Int = 0
+    var validation: ((Int, ItemStack) -> Boolean)? = null
+
+    constructor() : super()
+    constructor(size: Int) : super(size)
+    constructor(stacks: NonNullList<ItemStack>?) : super(stacks)
+
+    override fun onLoad() {
+        loadListener.forEach { it() }
+    }
+
+    override fun onContentsChanged(slot: Int) {
+        contentChangedListener.forEach { it(slot) }
+        hashInternal++
+    }
+
+    override fun getHash(): Int = hashInternal
+    override fun isItemValid(slot: Int, stack: ItemStack): Boolean {
+        return validation?.invoke(slot, stack) ?: true
+    }
+}
